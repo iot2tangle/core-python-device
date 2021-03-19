bc26a3e010378d2ea1d9ed063d59627a31a329d0
Gustavo Belbruno <gustavobelbruno@gmail.com>
2021-03-19 12:58:47 -0300

    class iot2tangle name changed to Sender - also, now Sender class accept also simple string, not only dictionary

--- iot2tangle.py
+++ iot2tangle.py
@@ -1,14 +1,16 @@
 import json
 import requests
 
-class iot2tangle:
+class Sender:
     def __init__(self, j):
-        self.data_json = j
+        if type(j) is type({}):
+            j=json.dumps(j)
+        self.data_json = j		# Json can be String or Dictionary
 
     def send_HTTP(self, url):
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         try:
-            r = requests.post(url, data=json.dumps(self.data_json), headers=headers)
+            r = requests.post(url, self.data_json, headers=headers)
             print("\t\t   Published in Channel: '" + r.text + "'\n")
         except:
             print("\t\t   --- HTTP Connection Error ---\n")
